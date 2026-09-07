2bf5133c0439b8c4b06d04f1787046d74d3b75d3
adell10 <ruaadelia@mail.ugm.ac.id>
2026-09-07 20:54:47 +0700

    feat: add tasks route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::get('/', function () {
     return view('welcome');
 });
